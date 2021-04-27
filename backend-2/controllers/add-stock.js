422fd6d3921c9751011007f769241a4236b914ad
billdelvin <billdelvin6@gmail.com>
2021-04-27 23:54:44 +0700

    clean code

--- backend-2/controllers/add-stock.js
+++ backend-2/controllers/add-stock.js
@@ -1,60 +1,60 @@
-const AddStockModels = require("../models/add-stock");
-const Dish = require("../models/dish")
+const AddStockModels = require('../models/add-stock');
+const Dish = require('../models/dish');
 
 module.exports = {
-  create: (req, res) => {
-    AddStockModels.create({
-        dish_id: req.body.dish_id,
-        quantity: req.body.quantity,  
-    })
-      .then((result) => res.json(result))
-      .catch((err) => {
-        throw err;
-      });
-  },
-  // getAll (show) berita.
-  getAllData: (req, res) => {
-    AddStockModels.findAll({
-      include:[
-        {
-          // model name which we want to include.
-          model: Dish, 
-          // we have to pass alias as we used while defining.
-          as: "dish",
-        }
-      ]
-    })
-      .then((result) => res.json(result))
-      .catch((err) => res.json(err));
-  },
-  // update byId.
-  updateDataById: (req, res) => {
-    AddStockModels.update(
-      {
-        dish_id: req.body.dish_id,
-        quantity: req.body.quantity,  
-      },
-      { where: { id: req.params.addId } }
-    )
-      .then((result) => res.json(result))
-      .catch((err) => {
-        throw err;
-      });
-  },
-  // delete berita by id.
-  deleteDataById: (req, res) => {
-    AddStockModels.destroy({ where: { id: req.params.addId } })
-      .then((result) => res.json(result))
-      .catch((err) => {
-        throw err;
-      });
-  },
-   // get berita by id.
-   getDataById: (req, res) => {
-    AddStockModels.findOne({ where: { id: req.params.addId } })
-      .then((result) => res.json(result))
-      .catch((err) => {
-        throw err;
-      });
-  }
+ create: (req, res) => {
+  AddStockModels.create({
+   dish_id: req.body.dish_id,
+   quantity: req.body.quantity,
+  })
+   .then((result) => res.json(result))
+   .catch((err) => {
+    throw err;
+   });
+ },
+ // getAll (show) berita.
+ getAllData: (req, res) => {
+  AddStockModels.findAll({
+   include: [
+    {
+     // model name which we want to include.
+     model: Dish,
+     // we have to pass alias as we used while defining.
+     as: 'dish',
+    },
+   ],
+  })
+   .then((result) => res.json(result))
+   .catch((err) => res.json(err));
+ },
+ // update byId.
+ updateDataById: (req, res) => {
+  AddStockModels.update(
+   {
+    dish_id: req.body.dish_id,
+    quantity: req.body.quantity,
+   },
+   { where: { id: req.params.addId } },
+  )
+   .then((result) => res.json(result))
+   .catch((err) => {
+    throw err;
+   });
+ },
+ // delete berita by id.
+ deleteDataById: (req, res) => {
+  AddStockModels.destroy({ where: { id: req.params.addId } })
+   .then((result) => res.json(result))
+   .catch((err) => {
+    throw err;
+   });
+ },
+ // get berita by id.
+ getDataById: (req, res) => {
+  AddStockModels.findOne({ where: { id: req.params.addId } })
+   .then((result) => res.json(result))
+   .catch((err) => {
+    throw err;
+   });
+ },
 };
